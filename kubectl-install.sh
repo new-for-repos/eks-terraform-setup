2673f3e21b7950dbb2e3f6ae097d5d36b6118317
Francess Alero Maku <francesmaku@gmail.com>
2022-05-12 19:14:48 -0400

    added kubectl-install.sh and edited terraform-install.sh

---
+++ kubectl-install.sh
@@ -0,0 +1,3 @@
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
--- terraform-install.sh
+++ terraform-install.sh
@@ -3,10 +3,8 @@
 sudo apt install wget unzip -y
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 sudo unzip terraform_0.12.26_linux_amd64.zip -d /usr/local/bin/
-#Export terraform binary path temporally
-export PATH=$PATH:/usr/local/bin
 #Add path permanently for current user.By Exporting path in .bashrc file at end of file.
- echo  "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
+echo  "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
 # Source .bashrc to reflect for current session
- source ~/.bashrc 
- terraform -version
+source ~/.bashrc
+terraform -version
